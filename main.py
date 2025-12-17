64f5c3f5c5430f40eeabc129fd7efb5e5c517739
Nasiasene <davi.ted@hotmail.com>
2025-12-17 10:17:47 -0300

    refactor: moving functions to utils

--- main.py
+++ main.py
@@ -1,50 +1,5 @@
 import sys
-from utils import Token, tipo_token, TipoToken
-
-def scan_tokens(conteudo):
-    tokens = []
-    pos = 0
-    
-    while pos < len(conteudo):
-        lexema = conteudo[pos]
-        posicao = pos
-        
-        if conteudo[pos].isdigit():
-            for p in range(pos+1, len(conteudo)):
-                if conteudo[p].isdigit():
-                    lexema += conteudo[p]
-                else:
-                    pos = p - 1
-                    break
-        elif conteudo[pos] == "#":
-            p = pos+1
-            while p < len(conteudo) and conteudo[p] != "\n":
-                lexema += conteudo[p]
-                p += 1
-            pos = p - 1
-        
-        tipo = tipo_token(lexema)
-        
-        if tipo == "WHITESPACE":
-            pos += 1
-            continue
-        
-        token = Token(tipo, lexema, posicao)
-        tokens.append(token)
-        pos += 1
-    
-    return tokens
-
-def verificar_e_reportar_erros(tokens):
-    erros = [token for token in tokens if token.tipo == TipoToken.ERROR]
-    
-    if erros:
-        print("Erros léxicos encontrados:")
-        for erro in erros:
-            print(f"Erro léxico na posição {erro.posicao}: '{erro.lexema}'")
-        return True
-    
-    return False
+from utils import Token, tipo_token, TipoToken,  scan_tokens, find_errors
 
 def main():
     if len(sys.argv) != 2:
@@ -62,7 +17,7 @@ def main():
     
     tokens = scan_tokens(conteudo)
     
-    if verificar_e_reportar_erros(tokens):
+    if find_errors(tokens):
         print("Compilação abortada devido a erros léxicos.")
         sys.exit(1)
     
--- utils.py
+++ utils.py
@@ -11,6 +11,16 @@ class TipoToken(Enum):
     COMENTARIO = "Coment"
     ERROR = "Error"
 
+class Token:
+    def __init__(self, tipo: TipoToken, lexema, posicao):
+        self.tipo = tipo
+        self.lexema = lexema
+        self.posicao = posicao
+
+    def __repr__(self):
+        return f"<{self.tipo.value}, '{self.lexema}', {self.posicao}>" 
+
+
 def tipo_token(strings):
     if strings.isdigit():
         return TipoToken.NUMERO
@@ -33,12 +43,47 @@ def tipo_token(strings):
     else:
         return TipoToken.ERROR
 
+def scan_tokens(conteudo):
+    tokens = []
+    pos = 0
+    
+    while pos < len(conteudo):
+        lexema = conteudo[pos]
+        posicao = pos
+        
+        if conteudo[pos].isdigit():
+            for p in range(pos+1, len(conteudo)):
+                if conteudo[p].isdigit():
+                    lexema += conteudo[p]
+                else:
+                    pos = p - 1
+                    break
+        elif conteudo[pos] == "#":
+            p = pos+1
+            while p < len(conteudo) and conteudo[p] != "\n":
+                lexema += conteudo[p]
+                p += 1
+            pos = p - 1
+        
+        tipo = tipo_token(lexema)
+        
+        if tipo == "WHITESPACE":
+            pos += 1
+            continue
+        
+        token = Token(tipo, lexema, posicao)
+        tokens.append(token)
+        pos += 1
+    
+    return tokens
 
-class Token:
-    def __init__(self, tipo: TipoToken, lexema, posicao):
-        self.tipo = tipo
-        self.lexema = lexema
-        self.posicao = posicao
-
-    def __repr__(self):
-        return f"<{self.tipo.value}, '{self.lexema}', {self.posicao}>" 
+def find_errors(tokens):
+    erros = [token for token in tokens if token.tipo == TipoToken.ERROR]
+    
+    if erros:
+        print("Erros léxicos encontrados:")
+        for erro in erros:
+            print(f"Erro léxico na posição {erro.posicao}: '{erro.lexema}'")
+        return True
+    
+    return False
